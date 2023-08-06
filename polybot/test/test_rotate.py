6801228d4c32badc3b2a0e242cf356160b32991e
edenb27 <edenblavat@gmail.com>
2023-08-06 19:49:05 +0300

    test fix

--- polybot/test/test_rotate.py
+++ polybot/test/test_rotate.py
@@ -1,5 +1,5 @@
 import unittest
-from ImageProcessingService.polybot.img_proc import Img
+from polybot.img_proc import Img
 import os
 
 img_path = 'polybot/test/beatles.jpeg' if '/polybot/test' not in os.getcwd() else 'beatles.jpeg'
